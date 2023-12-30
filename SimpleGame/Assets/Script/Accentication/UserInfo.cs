9223dfd28021c0c662f0cf8692adca8c44ae9e76
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-12-30 20:31:27 +0900

    스크립트 파트 : 사용되지 않는 변수 제거하기

--- SimpleGame/Assets/Script/Accentication/UserInfo.cs
+++ SimpleGame/Assets/Script/Accentication/UserInfo.cs
@@ -2,7 +2,6 @@
 public static class UserInfo
 {
     // get,set 자동 구현
-    public static long Id { get; set; }
     public static string Email { get; set; }
     public static string Password { get; set; }
     public static string Nickname { get; set; }
--- SimpleGame/Assets/Script/GameCharacter/GameCharacterInfo.cs
+++ SimpleGame/Assets/Script/GameCharacter/GameCharacterInfo.cs
@@ -4,7 +4,6 @@
 public static class GameCharacterInfo 
 {
     // get,set 자동 구현
-    public static long Id { get; set; }
     public static string Email { get; set; }
     public static string Nickname { get; set; }
     public static long HighScore { get; set; }
